7e06add335dcdad420e3bd0e7285579fb9013e50
Isaac Eduardo Amezcua Bustos <iamezcua.dev@gmail.com>
2023-12-26 15:34:00 -0600

    Fix class for SpringApplication

--- springio_building_a_restful_web_service/src/main/java/com/example/restservice/RestServiceApplication.java
+++ springio_building_a_restful_web_service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(GreetingController.class, args);
+        SpringApplication.run(RestServiceApplication.class, args);
     }
+
 }
